bf09a4ef9a3ebdbaa465d35ecab1bd9ddebbc5b9
RR <rrn091301@gmail.com>
2025-02-20 20:20:15 +0800

    feat: Add operator buttons functionality

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const currentNumber = document.querySelector("#currentNumber");
 const operatorContainer = document.querySelector("#operatorContainer");
 
 let operationContent;
+let operationNumber;
 let number = "0";
+let operator;
+let resetNumber;
+let result;
 const operands = [0,1,2,3,4,5,6,7,8,9];
 
 currentNumber.textContent = number;
@@ -30,6 +34,10 @@ operatorContainer.addEventListener("click", (event) => {
     if (operands.includes(+target.id)){
         if (number[0] === "0" && number[1] !== "."){
             number = "";
+        }
+        if (resetNumber){
+            number = "";
+            resetNumber = false;
         }
         number += target.id;
         if (number === ""){
@@ -58,7 +66,65 @@ operatorContainer.addEventListener("click", (event) => {
             break;
         case "clear":
             number = "0";
+            resetNumber = false;
+            operation.textContent = "";
+            operator = "";
             break;
+        case "+":
+            operationNumber = number;
+            operator = "+";
+            resetNumber = true;
+            operation.textContent = operationNumber.toString().concat("+");
+            break;
+        case "-":
+            operationNumber = number;
+            operator = "-";
+            resetNumber = true;
+            operation.textContent = operationNumber.toString().concat("-");
+            break;
+        case "x":
+            operationNumber = number;
+            operator = "x";
+            resetNumber = true;
+            operation.textContent = operationNumber.toString().concat("x");
+            break;
+        case "/":
+            operationNumber = number;
+            operator = "/";
+            resetNumber = true;
+            operation.textContent = operationNumber.toString().concat("/");
+            break;
+        case "equal":
+            switch (operator){
+                case "+":
+                    result = +operationNumber + +number;
+                    operation.textContent = operationNumber.toString().concat("+", number, "=");
+                    operationNumber = number;
+                    number = result;
+                    resetNumber = true;
+                    break;
+                case "-":
+                    result = +operationNumber - +number;
+                    operation.textContent = operationNumber.toString().concat("-", number, "=");
+                    operationNumber = number;
+                    number = result;
+                    resetNumber = true;
+                    break;
+                case "x":
+                    result = +operationNumber * +number;
+                    operation.textContent = operationNumber.toString().concat("x", number, "=");
+                    operationNumber = number;
+                    number = result;
+                    resetNumber = true;
+                    break;
+                case "/":
+                    result = +operationNumber / +number;
+                    operation.textContent = operationNumber.toString().concat("/", number, "=");
+                    operationNumber = number;
+                    number = result;
+                    resetNumber = true;
+                    break;
+            }       
     }
     currentNumber.textContent = number;
 });
